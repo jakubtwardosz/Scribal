92635f4fadce229fd4494005bcaef48a3ca01a72
Jakub Twardosz <jakubtwardosz@protonmail.com>
2024-02-12 04:23:15 +0100

    Create AuthController and user model.

---
+++ ScribalReactApp.Server/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ScribalReactApp.Server
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        
+    }
+}
